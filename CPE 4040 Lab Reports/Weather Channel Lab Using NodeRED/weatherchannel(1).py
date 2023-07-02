20c3055ef8ed2363cd862a34ba15b425c3cb7711
Clarence Barron <122894393+ClarenceBarron@users.noreply.github.com>
2023-07-02 00:29:25 -0400

    Add files via upload

---
+++ CPE 4040 Lab Reports/Weather Channel Lab Using NodeRED/weatherchannel(1).py
@@ -0,0 +1,100 @@
+"""app.py"""
+
+import boto3
+import datetime
+import time
+import json
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+
+AWS_S3_BUCKET = "tempersensorbucket" #edit this
+AWS_S3_KEY_PREFIX = "tempkey" #edit this NOTE: refers to s3 object key
+													 #not access key
+s3 = boto3.resource("s3")
+bucket = s3.Bucket(AWS_S3_BUCKET)
+
+#df = pd.DataFrame([data])
+#time in seconds between updates
+interval = 10
+
+def get_data_aws(s3bucket):
+	#get data from S3 bucket and return in dictionary format
+    obj = s3.Object(AWS_S3_BUCKET,AWS_S3_KEY_PREFIX)
+    data = obj.get()['Body'].read().decode('utf-8')
+    json_content = json.loads(data)
+		#key = obj.key
+		#body = obj.get()['Body'].read().decode('utf-8')
+		#json_content = json.loads(body)
+    return json_content
+
+#Get an initial reading
+data = get_data_aws(bucket)
+df = pd.DataFrame([data])
+#df = pd.DataFrame()
+#Set title of webpage and set up 2 empty containers
+st.set_page_config(layout="wide")
+st.title('CPE 4040 Weather Station')
+placeholder = st.empty()
+
+while True:
+    data = get_data_aws(bucket)
+    new_df = pd.DataFrame([data])
+    df = pd.concat([df, new_df], ignore_index=True)
+    #df = pd.DataFrame([data])
+    df = df.tail(24)
+    print(new_df)
+    #time.sleep(10)
+
+    current_temp = float(df['temp'].tail(1))
+    current_humidity = float(df['humidity'].tail(1))
+
+    with placeholder.container():
+		#3 columns for metrics under title
+        metric1, metric2, metric3 = st.columns(3)
+
+        metric1.metric(
+    			label = 'Current Temp (C)',
+    			value = current_temp)
+
+        metric2.metric(
+    			label = 'Current Humidity (%)',
+    			value = current_humidity)
+
+        metric3.metric(
+			label = 'Average Temp (C)',
+			value = round(pd.to_numeric(df['temp']).mean(),2))
+
+		#2 columns for line charts
+        fig_col1, fig_col2 = st.columns(2)
+
+        with fig_col1:
+			#generate a line-chart of the temp values
+            temp_chart = px.line(df,x='timestamp',y='temp',title='temp')
+			#get rid of ticks on x-axis because unix timestamps aren't readable
+            temp_chart.update_xaxes(showticklabels=False)
+			#explicitly set y-axis scaling and increase font-size
+            temp_chart.update_yaxes(range=[0, 100])
+            st.write(temp_chart)
+
+        with fig_col2:
+            humidity_chart = px.line(df,x='timestamp',y='humidity',title='humidity')
+            humidity_chart.update_xaxes(showticklabels=False)
+            humidity_chart.update_yaxes(range=[0, 100])
+            st.write(humidity_chart)
+
+        gauge_chart = go.Figure(go.Indicator(
+            mode   = 'gauge+number+delta',
+            value  = int(current_humidity),
+            domain = {'x':[0,1],'y':[0,1]},
+            delta  = {'reference':50},
+            title  = {'text':'Humidity'},
+            gauge  = {'axis':   {'range':[0,100]},
+                      'steps': [{'range': [0,50],  'color':'yellow'},
+                     {'range': [50,75], 'color':'orange'},
+                     {'range': [75,100],'color':'red'   }],}))
+        st.write(gauge_chart)
+
+    time.sleep(interval)
